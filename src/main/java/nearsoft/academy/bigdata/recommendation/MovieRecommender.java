efd6965151a1b08dd2d79a45b37e479fe8348a19
andrescst <acastellanos@nearsoft.com>
2019-10-03 21:34:39 -0500

    Created MovieRecommender class

---
+++ src/main/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -0,0 +1,33 @@
+package nearsoft.academy.bigdata.recommendation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieRecommender {
+
+
+    public MovieRecommender(String path){
+        //TODO: Read file from resources folder
+        //TODO: unzip file from gz to txt
+        //TODO: Read file with bufferedreader -  best option?
+    }
+
+    public int getTotalReviews() {
+        return 1;
+    }
+
+    public int getTotalProducts() {
+        return 1;
+    }
+
+    public int getTotalUsers() {
+        return 1;
+    }
+
+    public List<String> getRecommendationsForUser() {
+        List<String> recommendations = new ArrayList<String>();
+
+
+        return recommendations;
+    }
+}
